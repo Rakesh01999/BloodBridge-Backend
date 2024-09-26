448d83798c05939389f7e6f3f8ab7a5f2c32d11d
Rakesh Biswas <rbiswas01999@gmail.com>
2024-09-27 02:18:37 +0600

    admin -user_management route, user-delete, user-admin, donation request, blood groups added

--- index.js
+++ index.js
@@ -51,6 +51,26 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     app.post('/information', async (req, res) => {
       const informations = req.body
       const result = await informationCollection.insertOne(informations)

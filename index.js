9097394ebc1a528a0076570e774411f4487f635f
nafis200 <nafisahamed14@gmail.com>
2024-09-27 01:01:07 +0600

    well done

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
-const port = process.env.PORT || 5000
-const cors = require('cors')
-const { default: axios } = require('axios');
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-  ],
-  credentials: true
-}))
-app.use(express.json())
-app.use(cookieParser())
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { default: axios } = require("axios");
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uuibjb3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -23,81 +22,95 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
-
 
 async function run() {
   try {
-    const userCollection = client.db('BloodBridgeDB').collection('users')
-    const informationCollection = client.db('BloodBridgeDB').collection('information')
-    const BloodGroupsCollection = client.db('BloodBridgeDB').collection('BloodGroups')
-  
-    app.post('/users', async(req, res) => {
+    const userCollection = client.db("BloodBridgeDB").collection("users");
+    const informationCollection = client
+      .db("BloodBridgeDB")
+      .collection("information");
+    const BloodGroupsCollection = client
+      .db("BloodBridgeDB")
+      .collection("BloodGroups");
+
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user.email }
-      const existingUser = await userCollection.findOne(query)
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
       if (existingUser) {
-        return res.send({ message: 'user already exists' })
+        return res.send({ message: "user already exists" });
       }
       const newUser = req.body;
-      const result = await userCollection.insertOne(newUser)
-      res.send(result)
-    })
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
 
-    app.get('/users', async(req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.post('/information',async(req,res)=>{
-        const informations = req.body
-        const result = await informationCollection.insertOne(informations)
-        res.send(result) 
-     })
-     app.get('/information', async(req, res) => {
+    app.post("/information", async (req, res) => {
+      const informations = req.body;
+      const result = await informationCollection.insertOne(informations);
+      res.send(result);
+    });
+    app.get("/information", async (req, res) => {
       const result = await informationCollection.find().toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    
-      app.get('/bloodGroups', async(req, res) => {
-        const result = await BloodGroupsCollection.find().toArray();
-        res.send(result)
-      })   
-      
-      app.patch('/bloodGroups',async(req,res)=>{
-         const {bloodgroup,quantity} = req.body
-         const query = {bloodGroup: bloodgroup}
-         let amount = parseInt(quantity)
-         console.log(bloodgroup,quantity);
-         
-         const updateDoc = {
-           $inc:{
-            bloodQuantity:amount
-           }
-         }
+    app.get("/bloodGroups", async (req, res) => {
+      const result = await BloodGroupsCollection.find().toArray();
+      res.send(result);
+    });
 
-         const result = await BloodGroupsCollection.updateOne(query,updateDoc)
-         res.send(result)
+    app.patch("/bloodGroups", async (req, res) => {
+      const { bloodgroup, quantity } = req.body;
+      const query = { bloodGroup: bloodgroup };
+      let amount = parseInt(quantity);
+      console.log(bloodgroup, quantity);
 
+      const updateDoc = {
+        $inc: {
+          bloodQuantity: amount,
+        },
+      };
 
-      })
+      const result = await BloodGroupsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+    app.patch("/bloodGroups1", async (req, res) => {
+      const { bloodgroup, Acc_quantity } = req.body;
+      const query = { bloodGroup: bloodgroup };
+      let amount = parseInt(Acc_quantity);
+      console.log(bloodgroup, Acc_quantity);
+
+      const updateDoc = {
+        $inc: {
+          bloodQuantity: -amount,
+        },
+      };
+
+      const result = await BloodGroupsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('BloodBridge is running');
-})
+app.get("/", (req, res) => {
+  res.send("BloodBridge is running");
+});
 
 app.listen(port, () => {
   console.log(`BloodBridge is running on port ${port}`);
-})
-
+});
